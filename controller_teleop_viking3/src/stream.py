5fe9bc9c84bf1357569756c4afeb90bd98481f40
slippersen <erik.sandal@live.no>
2018-04-22 18:16:45 +0200

    zed stream utkast

---
+++ controller_teleop_viking3/src/stream.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# https://blog.miguelgrinberg.com/post/video-streaming-with-flask
+
+from flask import Flask, render_template, Response
+import time
+
+app = Flask(__name__)
+
+HOST = '192.168.1.3'	# on-board computer's IP address
+PORT = 8080
+
+
+@app.route('/')
+def index():
+	return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen():
+	"""Video streaming generator function."""
+	global img
+	while True:
+		try:
+			with open('frame.jpeg', 'rb') as f:
+				time.sleep(0.05) # 20fps
+				yield(b'--frame\r\n'
+					b'Content-Type: image/jpeg\r\n\r\n' + f.read() + b'\r\n')
+		except Exception as e:
+			print(e)
+
+
+if __name__ == '__main__':
+	app.run(host=HOST, port=PORT, threaded=True)
---
+++ controller_teleop_viking3/src/zed_sub.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# ROS imports
+import rospy
+from sensor_msgs.msg import Image
+
+# Utils
+import numpy as np
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+
+def callback(data):
+	"""
+	param data:	data from zed/rgb/image_rect_color topic
+	"""
+	# convert from ROS sensor_msgs/Image to cv2
+	bridge = CvBridge()
+	try:
+		cv_img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+		cv2.imwrite('frame.jpeg', cv_img)
+	except CvBridgeError as e:
+		print(e)
+	# show image stream
+	# cv2.imshow('zed', cv_img)
+	# cv2.waitKey(3)
+
+
+def power_control():
+	# initialize ROS node 'zed_sub'
+	rospy.init_node('zed_sub')
+	# subscribe to the ROS topic 'zed/rgb/image_rect_color'
+	rospy.Subscriber('zed/rgb/image_rect_color', Image, callback)
+	# keep python from exiting until this node is stopped
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+	power_control()
